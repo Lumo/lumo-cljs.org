3269c401f4bf30f29ae2565faf319860efb6f74b
jiyinyiyong <jiyinyiyong@gmail.com>
2018-03-30 09:09:28 +0800

    adding content

--- src/app/comp/container.cljs
+++ src/app/comp/container.cljs
@@ -10,7 +10,9 @@
             [reel.comp.reel :refer [comp-reel]]
             [respo-md.comp.md :refer [comp-md comp-md-block]]
             [app.schema :refer [dev?]]
-            [respo.util.list :refer [map-with-idx]]))
+            [respo.util.list :refer [map-with-idx]]
+            ["highlight.js" :as hljs]
+            ["escape-html" :as escape-html]))
 
 (defcomp
  comp-hero
@@ -22,13 +24,14 @@
    (div
     {:style {:background-image "url(http://cdn.tiye.me/logo/lumo.png)",
              :background-size :cover,
+             :background-position :center,
              :width 200,
              :height 200}})
    (=< 80 nil)
    (div
     {:style {:padding "8px 16px", :background-color (hsl 230 80 70 0.2)}}
     (comp-md-block
-     "```\n$ npm install -g lumo-cljs\n$ lumo\nLumo 1.7.0\nClojureScript 1.9.908\nNode.js v8.4.0\n\ncljs.user=> (println (+ 1 2 3))\n6\nnil\ncljs.user=>\n```"
+     "```\n$ npm install -g lumo-cljs\n$ lumo\nLumo 1.8.0\nClojureScript 1.9.946\nNode.js v9.2.0\n\ncljs.user=> (println (+ 1 2 3))\n6\nnil\ncljs.user=>\n```"
      {:style {},
       :css "pre code {\ncolor: white;\nline-height: 1.6em;\nfont-family: Source Code Pro, Menlo, Courier, monospace;\n}"})))
   (=< nil 40)
@@ -37,18 +40,24 @@
    (<>
     "Lumo is a standalone ClojureScript environment that runs on Node.js and the V8 JavaScript engine."))))
 
+(def supported-langs #{"clojure" "bash" "js"})
+
 (defcomp
  comp-intro
  ()
  (div
   {:style (merge ui/center {:font-size 16, :padding 16})}
   (comp-md-block
-   "Lumo has many features. I just don't have enough time to list all of them..."
-   {})))
+   "Lumo is a standalone ClojureScript environment that runs on Node.js and the V8 JavaScript engine. It starts up instantaneously and has out-of-the-box access to the entire Node.js ecosystem.\n\nTo install:\n\n```bash\n$ brew install lumo\n# or\n$ npm install -g lumo-cljs\n```\n\nAfter installed, you will a CLI tool called `lumo`.\n\n### REPL\n\n```bash\n=>> lumo\nLumo 1.8.0\nClojureScript 1.9.946\nNode.js v9.2.0\n Docs: (doc function-name-here)\n       (find-doc \"part-of-name-here\")\n Source: (source function-name-here)\n Exit: Control+D or :cljs/quit or exit\n\ncljs.user=> (println (+ 1 2 3))\n6\nnil\ncljs.user=>\n```\n\n### Node.js & npm modules\n\nAccess Node.js built-in variables with js interop:\n\n```clojure\n(.log js/console js/process.argv)\n```\n\nLoad npm modules from `node_modules/` with `js/require`:\n\n```bash\ncljs.user=> (def escape-html (js/require \"escape-html\"))\n#'cljs.user/escape-html\ncljs.user=> (escape-html \"<div />\")\n\"&lt;div /&gt;\"\n```\n\nAlso possible to specify dependencies in side `ns` form:\n\n```clojure\n(ns app.main (:require [\"fs\" :as fs]))\n\n(fs/readFileSync \"package.json\" \"utf8\")\n```\n\n### Interpreter\n\nRun a file\n\n```bash\n$ lumo main.cljs\n```\n\n### CLI arguments\n\nTo reaRead\n\n```bash\n$ lumo -h\n```\n\n### More...\n\nFind out more on [Wiki](https://github.com/anmonteiro/lumo/wiki)..."
+   {:highlight (fn [code lang]
+      (if (contains? supported-langs lang)
+        (.-value (.highlight hljs lang code))
+        (escape-html code)))})))
 
 (def site-links
   [{:title "GitHub", :url "https://github.com/anmonteiro/lumo"}
-   {:title "Blog", :url "https://anmonteiro.com/"}])
+   {:title "Blog", :url "https://anmonteiro.com/"}
+   {:title "Wiki", :url "https://github.com/anmonteiro/lumo/wiki"}])
 
 (defcomp
  comp-links-bar
--- src/app/main.cljs
+++ src/app/main.cljs
@@ -7,7 +7,11 @@
             [reel.util :refer [listen-devtools!]]
             [reel.core :refer [reel-updater refresh-reel]]
             [reel.schema :as reel-schema]
-            [cljs.reader :refer [read-string]]))
+            [cljs.reader :refer [read-string]]
+            ["highlight.js" :as hljs]
+            ["highlight.js/lib/languages/clojure" :as clojure-lang]
+            ["highlight.js/lib/languages/bash" :as bash-lang]
+            ["highlight.js/lib/languages/javascript" :as javascript-lang]))
 
 (defonce *reel
   (atom (-> reel-schema/reel (assoc :base schema/store) (assoc :store schema/store))))
@@ -24,6 +28,9 @@
 (def ssr? (some? (js/document.querySelector "meta.respo-ssr")))
 
 (defn main! []
+  (.registerLanguage hljs "clojure" clojure-lang)
+  (.registerLanguage hljs "bash" bash-lang)
+  (.registerLanguage hljs "js" javascript-lang)
   (if ssr? (render-app! realize-ssr!))
   (render-app! render!)
   (add-watch *reel :changes (fn [] (render-app! render!)))
