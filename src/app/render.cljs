b2a9a2fb7831af549e353586cc569df540f6161a
ChenYong <chenyong@jimeng.io>
2018-03-30 10:43:16 +0800

    support editing styles in workspace

--- src/app/render.cljs
+++ src/app/render.cljs
@@ -5,26 +5,22 @@
             [app.comp.container :refer [comp-container]]
             [app.schema :as schema]
             [reel.schema :as reel-schema]
-            [cljs.reader :refer [read-string]]
-            [app.schema :refer [dev?]]))
+            [cljs.reader :refer [read-string]]))
 
 (def base-info
   {:title "Lumo: Fast, cross-platform, standalone ClojureScript environment.",
    :icon "http://cdn.tiye.me/logo/lumo.png",
    :ssr nil,
-   :inline-html nil,
-   :inline-styles [(if dev? "" (slurp "./entry/main.css"))
-                   (slurp "./node_modules/highlight.js/styles/github-gist.css")]})
+   :inline-html nil})
 
 (defn dev-page []
   (make-page
    ""
    (merge
     base-info
-    {:styles (if dev?
-       ["http://localhost:8100/main.css" "/entry/main.css"]
-       ["http://localhost:8100/main.css"]),
-     :scripts ["/main.js"]})))
+    {:styles ["http://localhost:8100/main.css" "/entry/main.css"],
+     :scripts ["/main.js"],
+     :inline-styles [(slurp "./node_modules/highlight.js/styles/github-gist.css")]})))
 
 (def preview? (= "preview" js/process.env.prod))
 
@@ -40,7 +36,9 @@
       base-info
       {:styles ["http://cdn.tiye.me/favored-fonts/main.css"],
        :scripts (map #(-> % :output-name prefix-cdn) assets),
-       :ssr "respo-ssr"}))))
+       :ssr "respo-ssr",
+       :inline-styles [(slurp "./entry/main.css")
+                       (slurp "./node_modules/highlight.js/styles/github-gist.css")]}))))
 
 (defn main! []
   (if (= js/process.env.env "dev")
